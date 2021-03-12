cb71e2aefccae8a833af8eba38bf6d676cdbb3ab
Edicson Abel <edicsonabelinfo@gmail.com>
2021-03-12 18:53:46 -0400

    🔧 Cambio en la configuración de Gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,56 +2,53 @@ module.exports = {
   pathPrefix: "/",
   siteMetadata: {
     title: `Edicson Abel`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `Soy un desarollador 💻 intentando ayudarte a mejorar tus habilidades a través de artículos, cursos, vídeos y tutoriales. Así que comencemos 😎.`,
     author: `@edicsonabel_`,
   },
   plugins: [
-  `gatsby-plugin-react-helmet`,
-  `gatsby-plugin-sass`,
-  {
-    resolve: `gatsby-source-filesystem`,
-    options: {
-      name: `images`,
-      path: `${__dirname}/src/images`,
-    },
-  },
-  {
-    resolve: `gatsby-source-filesystem`,
-    options: {
-      name: `posts`,
-      path: `${__dirname}/src/posts`,
-    },
-  },
-  `gatsby-transformer-sharp`,
-  `gatsby-plugin-sharp`,
-  {
-    resolve: `gatsby-plugin-mdx`,
-    options:{
-      extensions: [`.md`, `.mdx`],
-      gatsbyRemarkPlugins: [
-      {
-        resolve: `gatsby-remark-images`,
-        options:{
-          maxWidth: 1200,
-        },
-      },
-      ],
-    },
-  },
-  {
-    resolve: `gatsby-plugin-manifest`,
-    options: {
-      name: `gatsby-starter-default`,
-      short_name: `starter`,
-      start_url: `/`,
-      background_color: `#001021`,
-      theme_color: `#001021`,
-      display: `minimal-ui`,
-        icon: `src/images/icon.svg`, // This path is relative to the root of the site.
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-    ],
-  }
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/posts`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.md`, `.mdx`],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1200,
+            },
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `⚡ Edicson Abel`,
+        short_name: `⚡EA`,
+        start_url: `/`,
+        background_color: `#242325`,
+        theme_color: `#242325`,
+        display: `minimal-ui`,
+        icon: `src/images/icon.svg`,
+      },
+    },
+  ],
+}
