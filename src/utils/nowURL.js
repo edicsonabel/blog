0f232c390c3ac69705cb2e444aa70330afb6bd96
Edicson Abel <edicsonabelinfo@gmail.com>
2022-02-22 02:32:36 -0400

    ⚙ Opción para ocultar pathname en nowURL

--- src/utils/nowURL.js
+++ src/utils/nowURL.js
@@ -1,12 +1,15 @@
 import { isBrowser } from './isBrowser'
 
-export const nowURL = () => {
+export const nowURL = (props) => {
   if (!isBrowser()) {
     return
   }
+
+  let showPathName = true
+  if(props?.pathname === false) {showPathName = false}
   const { protocol, host, pathname } = window.location
   let URL = protocol
   URL += host ? '//' + host : ''
-  URL += pathname ? pathname : ''
+  URL += pathname ? showPathName ? pathname : '' : ''
   return URL
 }
